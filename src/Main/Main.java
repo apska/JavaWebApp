34d736cef0e106f125d13473a916f8f2efd39944
apska <apska@mail.ru>
2016-02-02 22:34:37 +0300

    Добавление файла. Test.

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+/**
+ * Created by APS2 on 02.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
