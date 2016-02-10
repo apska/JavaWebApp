54a5c091fac459b582a0353b924146403e76eb47
apska <apska@mail.ru>
2016-02-10 22:50:12 +0300

    Reflection sample

--- src/lesson03/Main.java
+++ src/lesson03/Main.java
@@ -2,15 +2,27 @@ package lesson03;
 
 import com.github.apska.webapp.model.Contact;
 import com.github.apska.webapp.model.ContactType;
+import com.github.apska.webapp.model.Link;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by APS2
  * on 10.02.2016
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         Contact c = new Contact(ContactType.PHONE, "12321");
         ContactType c2 = ContactType.ICQ;
         //System.out.println(ContactType.ICQ == c2 );
+
+        Field f = Link.class.getDeclaredField("url");
+        f.setAccessible(true);
+
+        Link l = new Link("weqeq", "URL");
+
+        System.out.println(f.get(l));
+
+        //System.out.println(l);
     }
 }
