2dfaf14936d4016685d50480e766e8b2f355bdbc
apska <apska@mail.ru>
2016-08-01 22:00:01 +0400

    Тест сервлета.

---
+++ src/com/github/apska/webapp/web/ResumeServlet.java
@@ -0,0 +1,27 @@
+package com.github.apska.webapp.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Created by APS2
+ * on 01.08.2016
+ */
+public class ResumeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        Writer w = response.getWriter();
+        String name = request.getParameter("name");
+        w.write("Тест сервлет: привет " + name);
+        w.close();
+    }
+}
